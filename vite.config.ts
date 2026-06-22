eacf2a72c4296f50893f2845a36550a54c9b3dab
Dao Tran <daotran@ankvn.com>
2026-06-22 11:42:57 +0700

    .
    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/module_taskZoneEditor/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
